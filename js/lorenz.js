04cf5fe561022158f8dac59840764f5aa5fef1f8
AlexShukhman <alex.jacob.shukhman@gmail.com>
2016-08-05 18:54:56 -0400

    fixed lorenz

--- js/lorenz.js
+++ js/lorenz.js
@@ -1,5 +1,17 @@
-function Lorenz(ctx, s, r, b, step, zoom, steps, randTF) {
-	ctx.fillStyle = "#000";
+function Lorenz() {
+	var lorenzCanvas = document.getElementById('LorenzCanvas');
+	this.ctx = lorenzCanvas.getContext("2d");
+	this.canvasData = this.ctx.getImageData(0, 0, window.innerWidth, 500);
+	this.s = 10;
+	this.r = 24;
+	this.b = 8 / 3;
+	var step = 500; //500 is average step size more is smoother, but shorter length
+	var zoom = 5; //5 works fine
+	var steps = 10000; // length of attractor
+	var randTF = true; // if random init condition or 0,0,0
+	console.log('starting Lorenz');
+	this.ctx.lineWidth = '1';
+	this.ctx.strokeStyle = "blue";
 	if (randTF) {
 		var point = {
 			x: 2 * Math.random() * zoom - zoom
@@ -14,29 +26,37 @@ function Lorenz(ctx, s, r, b, step, zoom, steps, randTF) {
 			, z: 0
 		};
 	}
-	for (var i; i < steps; i++) {
-		point = lorenzRepeater(ctx, point, s, r, b, zoom, step);
-	}
+	console.log('starting for loop, steps = ' + steps);
+	var iterations = 0;
+	setInterval(function () {
+		if (iterations <= steps) {
+			point = lorenzRepeater(point, zoom, step);
+			iterations++;
+		}
+	}, 1000 / 1500);
 
 	function adjustX(zoom, x) {
-		return zoom * x + window.innerWidth / 2;
+		return zoom * x + (this.ctx.canvas.width / 2);
 	}
 
 	function adjustY(zoom, y) {
-		return zoom * y + 500 / 2;
+		return zoom * y + this.ctx.canvas.height / 2;
 	}
 
-	function drawPoint(ctx, zoom, x, y) {
+	function drawPoint(zoom, x, y) {
 		x = adjustX(zoom, x);
 		y = adjustY(zoom, y);
-		ctx.fillRect(x, y, 1, 1);
+		this.ctx.fillRect(x, y, 1, 1);
 	}
 
-	function lorenzRepeater(ctx, point, s, r, b, zoom, step) {
+	function lorenzRepeater(point, zoom, step) {
 		var x = point.x;
 		var y = point.y;
 		var z = point.z;
-		drawPoint(ctx, zoom, x, y);
+		var s = this.s;
+		var r = this.r;
+		var b = this.b;
+		drawPoint(zoom, x, y);
 		var deltaX = s * (y - x) / step;
 		var deltaY = (x * (r - z) - y) / step;
 		var deltaZ = (x * y - b * z) / step;
