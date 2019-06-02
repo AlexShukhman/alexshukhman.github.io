bd0f3c5f825a890771bac0a451b2aa96d3e59aa8
AlexShukhman <alex.jacob.shukhman@gmail.com>
2019-06-02 19:25:57 -0400

    first go

---
+++ build.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+'''
+Welcome to my personal website!
+
+As I was building this website, I realized that I wanted to handle updates scalably. To me, that meant having a sort of IMS (information management system) which I could easily update as I updated my resume. This is the software that builds the site's parts after they are updated.
+
+Recommended Environment:
+* Python 3 -- Anaconda: https://www.anaconda.com/download/
+'''
+
+print('Hello World!')
---
+++ public/include.js
@@ -0,0 +1,6 @@
+// Just a bunch of JQuery load statements to make building easier
+$('#title').load('./public/html/tile.html');
+$('#s1').load('./public/html/intro.html');
+$('#s2').load('./public/html/skills.html');
+$('#s3').load('./public/html/experience.html');
+$('#footer').load('./public/html/contact.html');
