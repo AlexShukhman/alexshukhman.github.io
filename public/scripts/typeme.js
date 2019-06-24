26ad56befdf10e4495cf3456f92bf951447a8e63
AlexShukhman <alex.jacob.shukhman@gmail.com>
2019-06-24 09:38:26 -0400

    footer

--- public/scripts/typeme.js
+++ public/scripts/typeme.js
@@ -1,37 +1,46 @@
 // Import
-var t;
-$.getJSON('/src/json/title.json').done((data) => {t=data;});
+var title;
+$.getJSON('/src/json/title.json').done((data) => {title=data;});
+
+var foot;
+$.getJSON('/src/json/contact.json').done((data) => {foot=data;});
 
 $(document).ready(()=>{
-    writeIn('#typeIn' ,0,0,0);
+    writeIn('#typeIn' ,0,0,0, 'title');
+    writeIn('#typeIn_foot', 0,0,0, 'foot')
 });
 
 var timeout = 100; // ms
 
-function writeIn(e, index, wd, wdI) {
-    if (!t) setTimeout(()=>{writeIn(e, 0,0,0);}, 200); // try again in 200 ms
+function writeIn(e, index, wd, wdI, t) {
+    if (!title && !foot) setTimeout(()=>{writeIn(e, 0,0,0, t);}, 200); // try again in 200 ms
     else {
+        console.log(t)
         if (wd == 0 && wdI == 0) setTimeout(()=>{
-            $(e).html('&#10003; '); // starting condition
-            _writeIn(e, index, wd, wdI);
+            if (t == 'title') $(e).html('&#10003; '); // starting condition
+            else $(e).html('');
+            _writeIn(e, index, wd, wdI, t);
         }, timeout+100);
-        else _writeIn(e, index, wd, wdI);
+        else _writeIn(e, index, wd, wdI, t);
     }
 }
 
-function _writeIn(e, index, wd, wdI) {
-    if (index == t.extras.length) {
-        writeIn(e, 0,0,0); // term. condition: restart (wait a bit for reader)
+function _writeIn(e, index, wd, wdI, type) {
+    if(type == "title") var t = title.extras;
+    else var t = foot.build_tools;
+
+    if (index == t.length) {
+        writeIn(e, 0,0,0, type); // term. condition: restart (wait a bit for reader)
     }
-    else if (wd == t.extras[index].split(" ").length) writeIn(e, index+1, 0,0); // term. condition: next phrase
-    else if (wdI == t.extras[index].split(" ")[wd].length) {
+    else if (wd == t[index].split(" ").length) writeIn(e, index+1, 0,0, type); // term. condition: next phrase
+    else if (wdI == t[index].split(" ")[wd].length) {
         $(e).append(' ');
-        writeIn(e, index, wd+1, 0); // term. condition: next word
+        writeIn(e, index, wd+1, 0, type); // term. condition: next word
     }
 
     else {
-        $(e).append(t.extras[index].split(" ")[wd].split("")[wdI]); // next letter
+        $(e).append(t[index].split(" ")[wd].split("")[wdI]); // next letter
 
-        setTimeout(()=>{writeIn(e, index, wd, wdI+1);}, timeout); // move on
+        setTimeout(()=>{writeIn(e, index, wd, wdI+1, type);}, timeout); // move on
     }
 }
--- src/python/contactBuilder.py
+++ src/python/contactBuilder.py
@@ -9,9 +9,13 @@ common = importlib.import_module('src.python.common')
 # ----- Local Helpers -----
 # Build HTML from JSON
 def build(fType, fName, j):
-    html = ""
+    html = "<table class='footer'><tr>"
 
-    
+    html += f"<td class='foot_first'>Built with <span id='typeIn_foot'></span></td>" 
+
+    html += f"<td>with {j['server']}</td>"
+
+    html += f"<td>by <a href='mailto:{j['email']}'>Alex</a></td></tr></table>" 
 
     return fType, fName, html
 
