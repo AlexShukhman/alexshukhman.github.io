03f4ecc2f36066c2f91b34a8517c539b835c4c18
AlexShukhman <alex.jacob.shukhman@gmail.com>
2019-07-07 15:05:45 -0400

    skills built out

--- src/python/experienceBuilder.py
+++ src/python/experienceBuilder.py
@@ -9,7 +9,18 @@ common = importlib.import_module('src.python.common')
 # ----- Local Helpers -----
 # Build HTML from JSON
 def build(fType, fName, j):
-    html = ""
+    t = "experience"
+    html = f"<hr><script>i.{t} = {str(len(j))};</script><p class='iconHeader'>"+t.capitalize() +"</p><div class='iconCont'>"
+    for i in range(len(j)):
+        html += f"<img class='icon iconImg' id='{t}Icon{str(i)}' alt='{j[i]['name']}' src='{j[i]['logo']}' onclick='updateTag(\"{t}\", {str(i)});'>"
+    html += "</div><div>"
+    for i in range(len(j)):
+        html += f'<div class="modal" id="{t}View{str(i)}">'
+        # Content
+        html += f"<p class='{t}Name'><a href='{j[i]['website']}'>{j[i]['name']}</a></p>"
+        # /Content
+        html += "</div>"
+    html += f"</div><script>updateTag('{t}', -1);</script>"
     return fType, fName, html
 
 
--- src/python/skillsBuilder.py
+++ src/python/skillsBuilder.py
@@ -9,7 +9,25 @@ common = importlib.import_module('src.python.common')
 # ----- Local Helpers -----
 # Build HTML from JSON
 def build(fType, fName, j):
-    html = ""
+    t = "skills"
+    html = f"<script>i.{t} = {str(len(j))};</script><p class='iconHeader'>"+t.capitalize() +"</p><div class='iconCont'>"
+    for i in range(len(j)):
+        html += f"<img class='icon iconImg' id='{t}Icon{str(i)}' alt='{j[i]['name']}' src='{j[i]['img']}' onclick='updateTag(\"{t}\", {str(i)});'>"
+    html += "</div><div>"
+    for i in range(len(j)):
+        html += f'<div class="modal" id="{t}View{str(i)}">'
+        # Content
+        html += f"<p class='{t}Name'>{j[i]['name']}</p>"
+        for e in ['fluent', 'semi', 'learning']:
+            if e in j[i].keys():
+                html += f"<div class='{t}List'>"
+                html += f"<p class='{t}Header {e}'>{e.capitalize()}</p>"
+                for skill in j[i][e]:
+                    html += f"<br><p class={t}body>{skill}</p>"
+                html += "</div>"
+        # /Content
+        html += "</div>"
+    html += f"</div><script>updateTag('{t}', -1);</script>"
     return fType, fName, html
 
 
