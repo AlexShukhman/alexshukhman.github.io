01893240425ebb38f2b5aa7e6ab242604795bc5c
alexshukhman <alex.jacob.shukhman@gmail.com>
2024-11-13 13:55:17 -0500

    updating content

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.get('/', (_req, res) => {
     res.sendFile(__dirname+'/index.html');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
